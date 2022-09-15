3e8461dcea7654f0305d607ec43ea79a34fcce47
Mashrab <yoldashovmashrab@gmail.com>
2022-09-15 12:06:01 +0500

    order products entity

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,17 @@
-import { Router } from 'express'
-import userController from '../controllers/user.controller'
+import { Router } from "express";
+import userController from "../controllers/user.controller";
+import orderProduct from "../controllers/order_products.controller";
 
-const router = Router()
+const router = Router();
 
 router
-    .get('/users', userController.GET)
-    .post('/users', userController.POST)
-    .post('/users', userController.POST)
-    .post('/users', userController.POST)
+  .get("/users", userController.GET)
+  .post("/users", userController.POST)
+  .post("/users", userController.POST)
+  .post("/users", userController.POST)
+  .get("/orderProduct", orderProduct.GET)
+  .post("/orderProduct", orderProduct.POST)
+  .delete("/orderProduct/:id", orderProduct.DELETE)
+  .put("/orderProduct/:id", orderProduct.PUT);
 
-export default router
+export default router;
